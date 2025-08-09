7e94dc5db478d54896bab4623e6e8cb3e15447d2
KosukeNakazaki <gokns.main@gmail.com>
2025-08-10 00:18:14 +0900

    Users修正

--- models/user.go
+++ models/user.go
@@ -14,19 +14,21 @@ const (
 )
 
 type User struct {
-	UUID          string    `json:"uuid" gorm:"primaryKey" validate:"required,uuid4"`
-	UserType      UserType  `json:"user_type" gorm:"type:varchar(20)" validate:"required,oneof=root general sub_general"`
-	Username      string    `json:"username" validate:"required,alphanum,min=3,max=30" gorm:"uniqueIndex"`
-	Password      string    `json:"password" validate:"required,min=8" gorm:"type:text"`
-	Email         string    `json:"email" validate:"required,email" gorm:"uniqueIndex"`
-	Lastname      string    `json:"lastname" validate:"required"`
-	Firstname     string    `json:"firstname" validate:"required"`
-	LastnameKana  string    `json:"lastname_kana" validate:"required,katakana"`
-	FirstnameKana string    `json:"firstname_kana" validate:"required,katakana"`
-	DepartmentID  string    `json:"department_id" gorm:"index"`
-	Remarks       string    `json:"remarks,omitempty"`
-	ManagedBy     string    `json:"managed_by,omitempty" gorm:"index"`
-	Verified      bool      `json:"verified"`
-	CreatedAt     time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt     time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	UUID          string     `json:"uuid" gorm:"type:uuid;primaryKey" validate:"required,uuid4"`
+	UserType      string     `json:"user_type" gorm:"type:varchar(20)" validate:"required,oneof=root general sub_general"`
+	Username      string     `json:"username" gorm:"type:varchar(30);index:idx_users_username,unique" validate:"required,alphanum,min=3,max=30"`
+	Password      string     `json:"password" gorm:"type:text" validate:"required,min=8"`
+	Email         string     `json:"email" gorm:"index:idx_users_email_nocase,unique,expression:lower(email)" validate:"required,email"`
+	Lastname      string     `json:"lastname" gorm:"type:varchar(100)" validate:"required"`
+	Firstname     string     `json:"firstname" gorm:"type:varchar(100)" validate:"required"`
+	LastnameKana  string     `json:"lastname_kana" validate:"required,katakana"`
+	FirstnameKana string     `json:"firstname_kana" validate:"required,katakana"`
+	DepartmentID  string     `json:"department_id" gorm:"type:uuid;index"`
+	Department    Department `gorm:"foreignKey:DepartmentID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
+	Remarks       string     `json:"remarks,omitempty"`
+	ManagedBy     string     `json:"managed_by,omitempty" gorm:"type:uuid;index"`
+	Manager       *User      `gorm:"foreignKey:ManagedBy;references:UUID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	Verified      bool       `json:"verified"`
+	CreatedAt     time.Time  `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt     time.Time  `json:"updated_at" gorm:"autoUpdateTime"`
 }
